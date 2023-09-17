204e6a470d752bab126da311bbafd669ac8e29fe
vjvelascorios <vj.velascorios@gmail.com>
2023-09-16 23:03:09 -0600

    update with tag

--- random_image.py
+++ random_image.py
@@ -1,27 +1,32 @@
 import os
 import random
 
-# Ruta al documento
+# Ruta al archivo que contiene tu documento
 documento_path = '/home/vjvelascorios/Documentos/GitHub/vjvelascorios/README.md'
 
-# Imágenes
+# Ruta al directorio de imágenes
 imagenes_dir = '/home/vjvelascorios/Documentos/GitHub/vjvelascorios/figures/'
 
 # Leer el contenido del documento
 with open(documento_path, 'r') as file:
     contenido = file.read()
 
-# Obtener la lista de archivos de imágenes en el directorio "images/"
-imagenes = [f for f in os.listdir(imagenes_dir) if os.path.isfile(os.path.join(imagenes_dir, f))]
+# Buscar la etiqueta '{{IMAGEN}}' en el contenido
+if '{{IMAGEN}}' in contenido:
+    # Obtener la lista de archivos de imágenes en el directorio "images/"
+    imagenes = [f for f in os.listdir(imagenes_dir) if os.path.isfile(os.path.join(imagenes_dir, f))]
 
-# Seleccionar una imagen al azar
-imagen_aleatoria = random.choice(imagenes)
+    # Seleccionar una imagen al azar
+    imagen_aleatoria = random.choice(imagenes)
 
-# Reemplazar la antigua línea de la imagen en el documento con la nueva imagen
-nuevo_contenido = contenido.replace('![Random photo here](figures/yes.jpeg)', f'![Random photo here]({imagen_aleatoria})')
+    # Reemplazar '{{IMAGEN}}' con la nueva imagen en el documento
+    nuevo_contenido = contenido.replace('{{IMAGEN}}', f'![Random photo here]({imagen_aleatoria})')
 
-# Escribir el contenido actualizado de vuelta al documento
-with open(documento_path, 'w') as file:
-    file.write(nuevo_contenido)
+    # Escribir el contenido actualizado de vuelta al documento
+    with open(documento_path, 'w') as file:
+        file.write(nuevo_contenido)
 
-print(f'Se ha cambiado la imagen a: {imagen_aleatoria}')
+    print(f'Se ha cambiado la imagen a: {imagen_aleatoria}')
+else:
+    print('La etiqueta {{IMAGEN}} no se encontró en el documento.')
+
